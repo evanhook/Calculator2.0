47b54f1e58ed322d288b18a1a3b7d2355e28c90d
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-03 13:54:39 -0700

    Add multiplication functionality to index.html and js

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ const divide = function(number1, number2) {
 //Multiplication Calculator
 
 const multi = function(number1, number2) {
-  return number1 + number2;
+  return number1 * number2;
   };
   
   $(document).ready(function() {
@@ -35,6 +35,13 @@ const multi = function(number1, number2) {
         const number1 = parseInt($("#sub1").val());
         const number2 = parseInt($("#sub2").val());
         const result = sub(number1, number2);
+        $("#output").text(result);
+    });
+    $("form#multiply").submit(function(event) {
+      event.preventDefault();
+        const number1 = parseInt($("#mul1").val());
+        const number2 = parseInt($("#mul2").val());
+        const result = multi(number1, number2);
         $("#output").text(result);
     });
   });
